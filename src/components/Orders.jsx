6efd27247acbc9f537995d561d24d90f1ec73bee
1zzat1lla-Fayzullayev <izzatillaf495@gmail.com>
2024-12-26 23:20:22 +0500

    change'

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -5,11 +5,10 @@ function Orders() {
     <>
       <div className="bg-[url('/order_bg.svg')] bg-cover bg-center h-full">
         <div className="text-center py-[77px]">
-          <p className="text-5xl md:text-6xl text-neutral-950 font-black">
-            <span className="text-neutral-50">Create</span> Your Dream Website
-            with Lyra
+          <p className="text-5xl md:text-6xl text-neutral-50 font-black">
+            Create Your Dream<span className="text-neutral-950"> Website with Lyra</span>
           </p>
-          <p className="text-neutral-800 mt-[20px]">
+          <p className="text-neutral-400 mt-[20px]">
             Submit your order today and let us craft a unique website tailored
             to your business needs. With Lyra, your vision comes to life✨
           </p>
