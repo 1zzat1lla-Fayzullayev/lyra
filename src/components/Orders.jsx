c7bb2756504d3547f979fbc9a7077cd80fde39f5
1zzat1lla-Fayzullayev <izzatillaf495@gmail.com>
2024-12-26 23:22:13 +0500

    change color

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -6,7 +6,7 @@ function Orders() {
       <div className="bg-[url('/order_bg.svg')] bg-cover bg-center h-full">
         <div className="text-center py-[77px]">
           <p className="text-5xl md:text-6xl text-neutral-50 font-black">
-            Create Your Dream<span className="text-neutral-950"> Website with Lyra</span>
+            Create Your Dream<span className="text-neutral-50 md:text-neutral-950"> Website with Lyra</span>
           </p>
           <p className="text-neutral-400 mt-[20px]">
             Submit your order today and let us craft a unique website tailored
