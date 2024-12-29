68b4307b25a11a992e44f31a5e604dc82bf39e90
1zzat1lla-Fayzullayev <izzatillaf495@gmail.com>
2024-12-29 08:53:14 +0500

    add founders

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -7,7 +7,9 @@ function AboutPage() {
     <>
       <div className="relative overflow-hidden">
         <div className="bg-neutral-950 text-neutral-50 h-screen flex flex-col justify-center items-center p-6 text-center">
-          <h1 className="text-center text-3xl md:text-6xl font-black text-neutral-50">Biz haqimizda</h1>
+          <h1 className="text-center text-3xl md:text-6xl font-black text-neutral-50">
+            Biz haqimizda
+          </h1>
 
           <FuzzyOverlay />
         </div>
@@ -35,6 +37,37 @@ function AboutPage() {
               Mijozlar ehtiyojlarini ustuvorlik bilan qondirish
             </li>
           </ul>
+        </div>
+
+        <div className="py-[50px] mx-[20px] xl:mx-0">
+          <h2 className="text-center text-3xl md:text-6xl font-black">
+            Asoschilar
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 mt-[50px] gap-8">
+            <a href="https://t.me/izzatilla_web" target="_blank">
+              <div className="flex flex-col items-center text-center md:border-r-[2px]">
+                <img
+                  src="/tg_logo.png"
+                  alt="Izzatilla Fayzullayev"
+                  className="w-[150px] h-[150px] rounded-full object-cover mb-4 shadow-lg"
+                />
+                <h3 className="text-xl font-bold">Izzatilla Fayzullayev</h3>
+                <p className="text-sm text-neutral-500">Founder & CEO</p>
+              </div>
+            </a>
+
+            <a href="https://t.me/undefined_coder" target="_blank">
+              <div className="flex flex-col items-center text-center">
+                <img
+                  src="/tg_logo.png"
+                  alt="Nizomiddin Nishonboyev"
+                  className="w-[150px] h-[150px] rounded-full object-cover mb-4 shadow-lg"
+                />
+                <h3 className="text-xl font-bold">Nizomiddin Nishonboyev</h3>
+                <p className="text-sm text-neutral-500">Founder & CEO</p>
+              </div>
+            </a>
+          </div>
         </div>
       </Wrapper>
     </>
